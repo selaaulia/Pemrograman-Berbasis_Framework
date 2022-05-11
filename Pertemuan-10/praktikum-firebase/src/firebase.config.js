92cfe511706f632cf63d8fd9a5f73bf670e0102e
selaaulia <selaaulia1307@gmail.com>
2022-05-11 20:28:24 +0700

    Pertamuan 10 Firebase Login

---
+++ Pertemuan-10/praktikum-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDKRyz5ADmMiMQGhEFN1PTV9y5Um-kNUdI",
+  authDomain: "praktikum-firebase-4f06e.firebaseapp.com",
+  projectId: "praktikum-firebase-4f06e",
+  storageBucket: "praktikum-firebase-4f06e.appspot.com",
+  messagingSenderId: "807468599564",
+  appId: "1:807468599564:web:48ea05d2ace18e35162129",
+  measurementId: "G-SZ3Q5NJC1P",
+};
